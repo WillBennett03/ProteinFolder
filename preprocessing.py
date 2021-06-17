02f2d586d31fd8e31bbe1e77fc1b587fa44211b9
WillBennett03 <Will1Bob2Bennett@gmail.com>
2021-06-17 23:00:19 +0100

    add files

--- preprocessing.py
+++ preprocessing.py
@@ -8,6 +8,13 @@
 Turns the proteinnet dataset into usable data for the model to make predictions from and train form.
 """
 import torch 
+import tokeniser
+import numpy as np
+
+def getPolypeptideVectors(PolypeptidesIter):
+    df_, dict_ = tokeniser.get_aminoacids()
+    x = [tokeniser.GetVectoredPolypeptide(x, dict_) for x in PolypeptidesIter]
+    return x
 
 def getRecord(filename):
     file = open(filename, 'r')#gets txt file
@@ -19,7 +26,7 @@ def getRecord(filename):
         if line[0] == '[':
             stage = line
         elif stage == "[PRIMARY]\n":
-            print(line)
+            # print(line)
             x.append(line[:-2])
         elif stage == "[EVOLUTIONARY]\n":
             pass
@@ -30,12 +37,15 @@ def getRecord(filename):
             if current != []:
                 y.append(current)
                 current = []
+
+    x = getPolypeptideVectors(x)
+    # y = torch.tensor(y)
     return x, y
 
     #print(file.read())
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #test code
     x,y = getRecord('protein_test')
     print(x)
     print(y)
